2b70afebc441cb4a615a9a4372c8c85a1c263794
Yuuta <yuutaman0424ku@gmail.com>
2025-07-20 11:32:04 +0900

    選択画面

--- frontend/src/Select.jsx
+++ frontend/src/Select.jsx
@@ -1,22 +1,18 @@
-import { useNavigate, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import './App.css';
 
-
 const Select = () => {
-	return(
-		<div>
-			<h1>プロンプト倉庫</h1>
-			<div>
-				<Link to="#" className="select-button">プロンプト一覧</Link>
-			</div>
-			<div>
-				<Link to="/create-ai" className="select-button">プロンプト作成(AI)</Link>
-			</div>
-			<div>
-				<Link to="#" className="select-button">プロンプト作成</Link>
-			</div>
-		</div>
-	)
-}
+  return (
+    <div className="select-container">
+      <h1 className="select-title">プロンプト倉庫</h1>
+
+      <div className="select-buttons">
+        <Link to="/prompts" className="select-button">プロンプト一覧</Link>
+        <Link to="/create-ai" className="select-button">プロンプト作成 (AI)</Link>
+        <Link to="/create" className="select-button">プロンプト作成 (手動)</Link>
+      </div>
+    </div>
+  );
+};
 
 export default Select;
